940b5c6f5139f0824caa67bfc820c35fc999cdd7
anwarul1 <anwarjewel007@gmail.com>
2022-05-08 21:57:39 +0600

    data corse rilated provlem

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+const corsConfig = {
+  origin: true,
+  credentials: true,
+};
+app.use(cors(corsConfig));
+app.options('*', cors(corsConfig));
 app.use(express.json());
 
 // mongodb
